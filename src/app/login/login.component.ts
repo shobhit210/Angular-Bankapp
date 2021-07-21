dbe171160c77a08c37fc3c21dfc8b6e0462a4765
Shobhit <shobhit.210@gmail.com>
2021-07-21 17:43:02 +0530

    login and routing of dashboard

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,49 +7,90 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  aim="Your daily banking partner";
+  aim = "Your daily banking partner";
 
 
-  acno=""
-  pswd=""
+  acno = ""
+  pswd = ""
 
-  user:any = {
-    1001:{acno:1001,uname:"shobhit",password:"userone",balance:3000},
-    1002:{acno:1002,uname:"natasha",password:"usertwo",balance:2000},
-    1003:{acno:1003,uname:"rohan",password:"userthree",balance:4000},
-    1004:{acno:1004,uname:"pooja",password:"userfour",balance:5000},
-    1005:{acno:1005,uname:"gargi",password:"userfive",balance:1000}
+  user: any = {
+    1001: { acno: 1001, uname: "shobhit", password: "userone", balance: 3000 },
+    1002: { acno: 1002, uname: "natasha", password: "usertwo", balance: 2000 },
+    1003: { acno: 1003, uname: "rohan", password: "userthree", balance: 4000 },
+    1004: { acno: 1004, uname: "pooja", password: "userfour", balance: 5000 },
+    1005: { acno: 1005, uname: "gargi", password: "userfive", balance: 1000 }
   }
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
-  accChange(event:any){
-    this.acno=event.target.value;
-  }
+  // accChange(event:any){
+  //   this.acno=event.target.value;
+  // }
 
-  pswdChange(event:any){
-    this.pswd=event.target.value;
-  }
+  // pswdChange(event:any){
+  //   this.pswd=event.target.value;
+  // }
 
 
 
-  login(){
-    var accnum=this.acno;
-    var paswrd=this.pswd;
-    let accDetails = this.user;
-    if(accnum in accDetails){
-      if(paswrd == accDetails[accnum]["password"]){
-        alert("Login Successful")
-      } else{
-        alert("Incorrect Password")
-      }
-    } else{
-      alert("Invalid account number")
+  // login(a:any,b:any){
+  //   // console.log(a);
+  //   // console.log(b);
+  //   var acno = a.value;
+  //   console.log(acno);
+  //   var pswd = b.value;
+  //   console.log(pswd);
+
+  //   let accDetails = this.user;
+
+  //   if (acno in accDetails){
+  //     if (pswd == accDetails[acno]["password"]){
+  //       alert("Login Successful");
+  //     } else {
+  //       alert("Incorrect Password");
+  //     }
+  //   } else{
+  //     alert("Invalid User")
+  //   }
+
+
+
+
+
+  // var accnum=this.acno;
+  // var paswrd=this.pswd;
+  // let accDetails = this.user;
+  // if(accnum in accDetails){
+  //   if(paswrd == accDetails[accnum]["password"]){
+  //     alert("Login Successful")
+  //   } else{
+  //     alert("Incorrect Password")
+  //   }
+  // } else{
+  //   alert("Invalid account number")
+  // }
+  // alert("Login Clicked");
+// }
+
+
+login(){
+  var accnum = this.acno;
+  var paswrd = this.pswd;
+  let accDetails = this.user;
+  if (accnum in accDetails) {
+    if (paswrd == accDetails[accnum]["password"]) {
+      alert("Login Successful")
+      this.router.navigateByUrl("dashboard");
+    } else {
+      alert("Incorrect Password")
     }
-    // alert("Login Clicked");
+  } else {
+    alert("Invalid account number")
   }
+  // alert("Login Clicked");
+}
   
 }
